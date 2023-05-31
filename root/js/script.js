af0badf45373b5398df4a7a4d0854e105b731ed5
abida-477 <128586817+abida-477@users.noreply.github.com>
2023-05-31 10:17:47 +0100

    added css

---
+++ root/js/script.js
@@ -0,0 +1,16 @@
+$(".button-1").click(function(){
+    $("section").not("#home").hide(300, "swing");
+    $("#screen-1").show(100, "swing")
+  });
+
+  $(".button-2").click(function(){
+    $("section").not("#selection").hide(300, "swing");
+    $("#screen-2").show(100, "swing")
+  });
+
+  $(".button-3").click(function(){
+    $("section").not("#selection").hide(300, "swing");
+    $("#screen-3").show(100, "swing")
+  });
+
+
